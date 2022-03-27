364265975521f3cb688b1d16c7246e0fd5c98dce
DennisUnplugged <80547849+DennisUnplugged@users.noreply.github.com>
2022-03-27 21:32:58 +0300

    Initial Commit

---
+++ linear_equation_solver/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  linear_equation_solver
+//
+//  Created by Dennis Gyftakis on 27/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
